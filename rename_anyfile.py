681a30953d948b37ffacb224b92a9390faf913e0
Takeuchi Haruhiko <noharu1205@gmail.com>
2020-07-14 15:33:43 +0900

    Add files via upload

--- rename_anyfile.py
+++ rename_anyfile.py
@@ -1,17 +1,29 @@
+"""
+Search for the file name in google and add it to the file name.
+ex)
+input OnePiece.pdf
+output [尾田 栄一郎] OnePiece.pdf
+"""
+#pip install lxml
 from urllib.parse import parse_qsl
 from urllib.parse import urlparse
-
+#pip install beautifulsoup4
 from bs4 import BeautifulSoup
+#pip install requests
 from requests import get as GET
 import re
 import os
 import sys
 import webbrowser
 
-repository_mame = "C:/Users/USER/Downloads/H/pdf/[5/新しいフォルダー"
+#The folder with the files you want to search. Multiple files are possible.
+folder_name="C:/Users/USER/Downloads/book" 
+#Keywords related to the file (to improve search hits) 
+keyword="manga"
 
-def idou(file):
-    html = GET("https://www.google.co.jp/search?q={} 同人".format(file)).text
+
+def idou(file,ext):
+    html = GET("https://www.google.co.jp/search?q={0} {1}".format(file,keyword)).text
     bs = BeautifulSoup(html, 'lxml')
     print("\n"+"-"*15+file+"-"*15)
     print(bs.find_all(class_="BNeawe vvjwJb AP7Wnd")[0].getText().strip())
@@ -20,29 +32,25 @@ def idou(file):
     print(bs.find_all(class_="BNeawe vvjwJb AP7Wnd")[3].getText().strip())
 
     for el in bs.select("h3.r a"):
-        #print(el)
         title = el.get_text()
-        #url = dict(parse_qsl(urlparse(el.get("href")).query))["q"]
         print(title)
-        #print(re.search(r"[", title))
-        #print(title)
-        #print("  ", url)
-    z = input()
-    if z == "a":
+    add_name = input()
+    if add_name == "a":
         pass
-        webbrowser.open("https://www.google.co.jp/search?q={}+同人".format(file))
-        z=""
-        z = input()
-        os.rename("{0}/{1}.pdf".format(repository_mame, file),
-                  "C:/Users/USER/Downloads/H/pdf/[5/ido/[{0}] {1}.pdf".format(z, file))
+        webbrowser.open("https://www.google.co.jp/search?q={0}+{1}".format(file,keyword))
+        add_name=""
+        add_name = input()
+        os.rename("{0}/{1}{2}".format(folder_name,file,ext),
+                  "{0}/[{1}] {2}{3}".format(folder_name,add_name, file,ext))
     else:
-        #os.rename("C:/Users/USER/Downloads/cg/5/{}.pdf".format(file),"C:/Users/USER/Downloads/cg/5/[{0}] {1}.pdf".format(z, file))
-        os.rename("{0}/{1}.pdf".format(repository_mame, file),
-                  "C:/Users/USER/Downloads/H/pdf/[5/ido/[{0}] {1}.pdf".format( z, file))
+        os.rename("{0}/{1}{2}".format(folder_name,file,ext),
+            "{0}/[{1}] {2}{3}".format(folder_name,add_name, file,ext))
 
-
-files = os.listdir("{0}".format(repository_mame))
-for i in files:
-    i = i[:-4]
-    idou(i)
-
+if __name__ == '__main__':
+    files = os.listdir(folder_name)
+    for _name in files:
+        root_ext = os.path.splitext(_name)[1]
+        cnt_ext=int(len(root_ext))
+        if cnt_ext>0:
+            _name = _name[:-1*cnt_ext]
+        idou(_name,root_ext)
